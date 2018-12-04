92b8c5d50c4ab5291044ca5cc0f12c317de056a3
mullerhai <hai710459649@foxmail.com>
2018-12-04 15:06:00 +0800

    Initial commit

---
+++ src/main/java/com/volunteer/config/SparkConfig.java
@@ -0,0 +1,4 @@
+package com.volunteer.config;
+
+public class SparkConfig {
+}
---
+++ src/main/java/com/volunteer/templatez/TemplatezApplication.java
@@ -0,0 +1,12 @@
+package com.volunteer.templatez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplatezApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TemplatezApplication.class, args);
+    }
+}
